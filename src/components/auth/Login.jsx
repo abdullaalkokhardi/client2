42589dec699854891a1c8da592cc5f2f60eb651f
abdullaalkokhardi <abdulla.alkokhardi@gmail.com>
2023-02-28 08:52:34 +0400

    adjust forms

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,18 +1,41 @@
 import React, { Component } from 'react'
 
 export default class Login extends Component {
+
+  constructor(){
+    super();
+    //call the base class contseuctor 
+    this.state={
+      username:"",
+      password:"",
+    };
+    // state: object from base class to hold data 
+   
+  }
+  onChange=(e)=>{
+    // need to collect data for changed fields and update the state
+    //console.log(e.target.value);
+    //console.log(e.target.name);
+    this.setState({[e.target.name]: e.target.value})
+  };
+  onSubmit=(e)=>{
+    e.preventDefault();
+    console.log(this.state);
+  };
   render() {
+    const { username, password}= this.state;
+
     return (
       <>
       <br />
         <section> 
     <h1 class="large text-primary">Login</h1>
-<form class="form" action="xyz.html">
+<form class="form" onSubmit={this.onSubmit}>
     <div class="form-group"> 
-    <input type="text" name="username" placeholder="Username"/>
+    <input type="text" name="username" placeholder="Username" value={username} onChange={this.onChange}/>
     </div>
     <div class="form-group">
-    <input type="password" name="password"  placeholder="Password"/>
+    <input type="password" name="password"  placeholder="Password" value={password} onChange={this.onChange}/>
     </div>
     <div class="form-group">
     <input type="submit" value="Login"/>
--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,40 +1,81 @@
 import React, { Component } from 'react'
+import api from '../../utils/api';
 
 export default class Register extends Component {
+  constructor(){
+    super();
+    //call the base class contseuctor 
+    this.state={
+      name:"",
+      email:"",
+      password:"",
+      confirmPassword:"", 
+    };
+    // state: object from base class to hold data 
+   
+  }
+  onChange=(e)=>{
+    // need to collect data for changed fields and update the state
+    //console.log(e.target.value);
+    //console.log(e.target.name);
+    this.setState({[e.target.name]: e.target.value})
+  };
+  onSubmit=(e)=>{
+    e.preventDefault();
+    console.log(this.state);
+    api.post('/users', this.state)
+    //  -end point -data -headers
+  };
+
   render() {
+    const { name, email, password, confirmPassword}= this.state;
     return (
       <>
-      <section class="container">
-      <h1 class="x-large text-primary">Sign Up</h1>
-      <p class="lead"><i class="fa fa-user-circle-o"></i>Create your Account</p>
-      <form class="form">
+       <section class="container">
+        <br />
+      <h1 class="large text-primary">Sign Up</h1>
+      <p class="lead"><i class="fas fa-user"></i> Create Your Account</p>
+      <form class="form" onSubmit={this.onSubmit}>
         <div class="form-group">
-          <input type="text" placeholder="Enter the Name" name="name" />
+          <input type="text" placeholder="Name" name="name" required value={name}
+          onChange={this.onChange}
+          />
         </div>
         <div class="form-group">
-          <input type="email" placeholder="enter your email" name="email" />
-          <small
+          <input type="email" placeholder="Email Address" name="email" value={email} 
+          onChange={this.onChange}/>
+          <small class="form-text"
             >This site uses Gravatar so if you want a profile image, use a
             Gravatar email</small
           >
         </div>
         <div class="form-group">
-          <input type="password" placeholder="Password" name="password" />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={this.onChange}
+            
+          />
         </div>
         <div class="form-group">
           <input
             type="password"
             placeholder="Confirm Password"
-            name="password2"
+            name="confirmPassword"
+            minLength="6"
+            value={confirmPassword}
+            onChange={this.onChange}
+            
           />
         </div>
-        <div class="form-group">
-          <input type="date" name="dob" placeholder="Enther DOB" />
-        </div>
-        <div class="form-group">
-          <input type="submit" class="btn btn-primary" value="Register" />
-        </div>
+        <input type="submit" class="btn btn-primary" value="Register" />
       </form>
+      <p class="my-1">
+        Already have an account? <a href="/login">Sign In</a>
+      </p>
     </section>
       </>
     )
